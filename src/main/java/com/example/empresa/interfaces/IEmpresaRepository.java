b6ac64d4019506613692285ea70dcd0bad178ebd
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2024-09-15 19:25:49 -0300

    asda

--- src/main/java/com/example/empresa/interfaces/IEmpresaRepository.java
+++ src/main/java/com/example/empresa/interfaces/IEmpresaRepository.java
@@ -1,9 +1,13 @@
 package com.example.empresa.interfaces;
 
 import com.example.empresa.entities.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface IEmpresaRepository extends JpaRepository<Empresa, Integer> {
+import java.util.List;
+
+public interface IEmpresaRepository {
+    List<Empresa> buscarTodos();
+    Empresa buscarPorId(int id);
+    Empresa gravar(Empresa empresa);
+    Empresa atualizar(int id, Empresa empresaAtualizada);
+    void excluir(int id);
 }
--- src/main/java/com/example/empresa/repositories/EmpresaRepository.java
+++ src/main/java/com/example/empresa/repositories/EmpresaRepository.java
@@ -1,44 +1,55 @@
 package com.example.empresa.repositories;
 
-import com.example.empresa.entities.Empresa; 
+import com.example.empresa.entities.Empresa;
 import com.example.empresa.interfaces.IEmpresaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class EmpresaRepository {
+@Repository
+public class EmpresaRepository implements IEmpresaRepository {
 
-    @Autowired
-    private IEmpresaRepository empresaRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    @Override
     public List<Empresa> buscarTodos() {
-        return empresaRepository.findAll();
+        String jpql = "SELECT e FROM Empresa e";
+        TypedQuery<Empresa> query = entityManager.createQuery(jpql, Empresa.class);
+        return query.getResultList();
     }
 
+    @Override
     public Empresa buscarPorId(int id) {
-        Optional<Empresa> empresa = empresaRepository.findById(id);
-        return empresa.orElse(null);
+        return entityManager.find(Empresa.class, id);
     }
 
+    @Override
     public Empresa gravar(Empresa empresa) {
-        return empresaRepository.save(empresa);
+        entityManager.persist(empresa);
+        return empresa;
     }
 
+    @Override
     public Empresa atualizar(int id, Empresa empresaAtualizada) {
-        Optional<Empresa> empresaExistente = empresaRepository.findById(id);
-        if (empresaExistente.isPresent()) {
-            Empresa empresa = empresaExistente.get();
+        Empresa empresa = entityManager.find(Empresa.class, id);
+        if (empresa != null) {
             empresa.setNome(empresaAtualizada.getNome());
             empresa.setCnpj(empresaAtualizada.getCnpj());
-            return empresaRepository.save(empresa);
+            return entityManager.merge(empresa);
         }
         return null;
     }
 
+    @Override
     public void excluir(int id) {
-        empresaRepository.deleteById(id);
+        Empresa empresa = entityManager.find(Empresa.class, id);
+        if (empresa != null) {
+            entityManager.remove(empresa);
+        }
     }
 }
