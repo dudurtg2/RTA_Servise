48033655986de29098ef7b72028749b0591f0625
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2024-09-19 14:29:00 -0300

    .

--- src/main/java/com/example/empresa/controllers/EmpresaController.java
+++ src/main/java/com/example/empresa/controllers/EmpresaController.java
@@ -1,8 +1,10 @@
 package com.example.empresa.controllers;
 
+import com.example.empresa.applications.EmpresaApplication;
 import com.example.empresa.entities.Empresa;
+import com.example.empresa.facades.EmpresaFacade;
 import com.example.empresa.repositories.EmpresaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,21 +13,30 @@ import java.util.List;
 @RequestMapping("/empresas")
 public class EmpresaController {
 
-    @Autowired
-    
-    private EmpresaRepository empresaRepository;
+    private static EmpresaRepository empresaRepository;
+    private static EmpresaApplication empresaApplication;
+    private static EmpresaFacade empresaFacade;
+
+    public static void resolverDependencias() {
+        empresaRepository = new EmpresaRepository();
+        empresaApplication = new EmpresaApplication(empresaRepository);
+        empresaFacade = new EmpresaFacade(empresaApplication);
+    }
+    public EmpresaController() {
+        resolverDependencias();
+    }
 
     @GetMapping("/buscarTodos")
     public List<Empresa> buscarTodos() {
-        return empresaRepository.buscarTodos();
+        return empresaFacade.buscarTodos();
     }
 
     @GetMapping("/buscarPorId/{id}")
     public Empresa buscarPorId(@PathVariable int id) {
-        return empresaRepository.buscarPorId(id);
+        return empresaFacade.buscarPorId(id);
     }
 
-    @GetMapping("/gravar")
+    @PostMapping("/gravar")
     public Empresa gravar(
             @RequestParam String nome,
             @RequestParam String cnpj) {
@@ -34,27 +45,27 @@ public class EmpresaController {
         novaEmpresa.setNome(nome);
         novaEmpresa.setCnpj(cnpj);
 
-        return empresaRepository.gravar(novaEmpresa);
+        return empresaFacade.gravar(novaEmpresa);
     }
 
-    @GetMapping("/atualizar/id={id}")
+    @PutMapping("/atualizar/{id}")
     public Empresa atualizar(
             @PathVariable int id,
             @RequestParam String nome,
             @RequestParam String cnpj) {
 
-        Empresa empresa = empresaRepository.buscarPorId(id);
+        Empresa empresa = empresaFacade.buscarPorId(id);
 
         if (empresa != null) {
             empresa.setNome(nome);
             empresa.setCnpj(cnpj);
-            return empresaRepository.atualizar(id, empresa);
+            return empresaFacade.atualizar(id, empresa);
         }
         return null;
     }
 
-    @GetMapping("/excluir/{id}")
+    @DeleteMapping("/excluir/{id}")
     public void excluir(@PathVariable int id) {
-        empresaRepository.excluir(id);
+        empresaFacade.excluir(id);
     }
 }
