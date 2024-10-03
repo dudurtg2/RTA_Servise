b86c1d0e1e6c2beebff50fd894bb909470e34b77
esterbiziu <esterbiziu@gmail.com>
2024-10-02 21:10:21 -0300

    Controller-Motorista

---
+++ src/main/java/com/example/empresa/controllers/MotoristaController.java
@@ -0,0 +1,65 @@
+package com.example.empresa.controllers;
+
+import com.example.empresa.entities.Motorista;
+import com.example.empresa.facades.MotoristaFacade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/motorista")
+public class MotoristaController {
+
+    private MotoristaFacade motoristaFacade;
+
+    @Autowired
+    public MotoristaController(MotoristaFacade motoristaFacade) {
+        this.motoristaFacade = motoristaFacade;
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Motorista>> findAll() {
+        List<Motorista> motoristas = this.motoristaFacade.findAll();
+
+        return new ResponseEntity<List<Motorista>>(motoristas, HttpStatus.OK);
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Motorista> findById(@PathVariable int id) {
+        Motorista motorista = this.motoristaFacade.findById(id);
+        
+        return new ResponseEntity<Motorista>(motorista, HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Motorista> save(@RequestBody Motorista motorista) {
+        Motorista motoristaSaved = motoristaFacade.save(motorista);
+
+        return new ResponseEntity<Motorista>(motoristaSaved, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Motorista> update(
+            @PathVariable int id,
+            @RequestBody Motorista motorista) {
+
+            Motorista motoristaUpdated = motoristaFacade.update(id, motorista);
+
+            if (motoristaUpdated == null)
+                return new ResponseEntity<Motorista>(motoristaUpdated, HttpStatus.NOT_FOUND);
+
+            return new ResponseEntity<Motorista>(motoristaUpdated, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable int id) {
+        motoristaFacade.deleteById(id);
+        
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+}
