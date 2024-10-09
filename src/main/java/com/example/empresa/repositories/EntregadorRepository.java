cfed7462812d896164ccfc7dda4890e14366c3d0
guilermePontes <pontes.design001@gmail.com>
2024-10-08 21:04:26 -0300

    repo

--- src/main/java/com/example/empresa/repositories/EntregadorRepository.java
+++ src/main/java/com/example/empresa/repositories/EntregadorRepository.java
@@ -2,42 +2,63 @@ package com.example.empresa.repositories;
 
 import com.example.empresa.entities.Entregador;
 import com.example.empresa.interfaces.IEntregadorRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public class EntregadorRepository implements IEntregadorRepository {
 
+   @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Entregador> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        String jpql = "SELECT b FROM Entregador b";
+        TypedQuery<Entregador> query = entityManager.createQuery(jpql, Entregador.class);
+        return query.getResultList();
     }
 
     @Override
     public Entregador findById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        return entityManager.find(Entregador.class, id);
     }
 
     @Override
+    @Transactional
     public Entregador save(Entregador entregador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        entityManager.persist(entregador);
+
+        return entregador;
     }
 
     @Override
+    @Transactional
     public Entregador update(int id, Entregador entregador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        Entregador entregadorInDb = entityManager.find(Entregador.class, id);
+        
+        entregadorInDb.setNome(entregador.getNome());
+        entregadorInDb.setEndereco(entregador.getEndereco());
+        entregadorInDb.setEmail(entregador.getEmail());
+        entregadorInDb.setCpf(entregador.getCpf());
+        entregadorInDb.setTelefone(entregador.getTelefone());
+
+        return entityManager.merge(entregadorInDb);
     }
 
     @Override
+    @Transactional
     public void deleteById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+        Entregador entregadorInDb = entityManager.find(Entregador.class, id);
+        if (entregadorInDb != null) {
+            entityManager.remove(entregadorInDb);
+        }
     }
 
-   
 }
