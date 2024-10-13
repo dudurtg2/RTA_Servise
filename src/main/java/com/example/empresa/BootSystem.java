e0e632fdd917fa357aed30258f8b5e280fea82b6
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2024-10-13 09:54:28 -0300

    .

--- src/main/java/com/example/empresa/BootSystem.java
+++ src/main/java/com/example/empresa/BootSystem.java
@@ -1,18 +1,6 @@
 package com.example.empresa;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.empresa.entities.Cidade;
-import com.example.empresa.entities.Empresa;
-import com.example.empresa.entities.Regiao;
-import com.example.empresa.facades.CidadeFacade;
-import com.example.empresa.facades.EmpresaFacade;
-import com.example.empresa.facades.RegiaoFacade;
-
 @SpringBootApplication
 public class BootSystem{
 
@@ -20,32 +8,5 @@ public class BootSystem{
 		SpringApplication.run(BootSystem.class, args);
 	}
 
-	private EmpresaFacade empresafacade;
-	private CidadeFacade cidadeFacade;
-	private RegiaoFacade regiaoFacade;
-
-	@Autowired
-	public BootSystem(EmpresaFacade empresafacade, CidadeFacade cidadeFacade, RegiaoFacade regiaoFacade) {
-		this.empresafacade = empresafacade;
-		this.cidadeFacade = cidadeFacade;
-		this.regiaoFacade = regiaoFacade;
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(CidadeFacade cidadeFacade, RegiaoFacade regiaoFacade) {
-		return runner -> {
-			saveRegiao(regiaoFacade);
-			saveCidade(cidadeFacade);
-		};
-	}
-	private void saveRegiao(RegiaoFacade regiaoFacade) {
-		Regiao regiao = new Regiao("Regiao de Teste");
-		regiaoFacade.save(regiao);
-	}
-	private void saveCidade(CidadeFacade cidadeFacade) {
-		Regiao regiao = regiaoFacade.findById(1);
-		Cidade cidade = new Cidade("Cidade de Teste", "0000000", regiao);
-		cidadeFacade.save(cidade);
-	}
 
 }
