77c00596559795ce1c17256601cfe3a3a4255a70
esterbiziu <esterbiziu@gmail.com>
2024-11-21 21:07:17 -0300

    JavaDoc Interfaces

--- src/main/java/com/example/empresa/interfaces/ICidadeRepository.java
+++ src/main/java/com/example/empresa/interfaces/ICidadeRepository.java
@@ -5,12 +5,51 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Interface de repositório para a entidade {@link Cidade}.
+ * Define os métodos básicos para operações CRUD (Create, Read, Update, Delete) no repositório.
+ * O repositório interage com a camada de persistência de dados.
+ */
 @Repository
 public interface ICidadeRepository {
+
+    /**
+     * Recupera todas as instâncias da entidade {@link Cidade}.
+     * 
+     * @return Uma lista contendo todas as instâncias de {@link Cidade}.
+     */
     List<Cidade> findAll();
+
+    /**
+     * Recupera uma instância de {@link Cidade} com base no identificador único.
+     * 
+     * @param id O identificador único da instância de {@link Cidade}.
+     * @return A instância de {@link Cidade} correspondente ao id, ou null se não encontrado.
+     */
     Cidade findById(int id);
+
+    /**
+     * Salva uma nova instância de {@link Cidade} no repositório.
+     * 
+     * @param cidade A instância de {@link Cidade} a ser salva.
+     * @return A instância de {@link Cidade} salva.
+     */
     Cidade save(Cidade cidade);
+
+    /**
+     * Atualiza uma instância existente de {@link Cidade}.
+     * 
+     * @param id O identificador único da instância a ser atualizada.
+     * @param cidade A nova instância de {@link Cidade} com as atualizações.
+     * @return A instância de {@link Cidade} atualizada, ou null se não encontrada.
+     */
     Cidade update(int id, Cidade cidade);
+
+    /**
+     * Exclui a instância de {@link Cidade} com base no identificador único.
+     * 
+     * @param id O identificador único da instância a ser excluída.
+     */
     void deleteById(int id);
 }
-   
+
